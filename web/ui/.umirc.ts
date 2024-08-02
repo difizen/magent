c9cbd128397ef4a0ea45c8c33cbacf0433c755a1
yukun.wyk <yukun.wyk@antgroup.com>
2024-08-02 13:10:39 +0800

    feat(ui): fix file loader runtime path

--- web/ui/.umirc.ts
+++ web/ui/.umirc.ts
@@ -1,6 +1,23 @@
 import { defineConfig } from 'umi';
 import path from 'path';
 import routes from './routes';
+
+class ExposeWebpackRequirePlugin {
+  apply(compiler) {
+    compiler.hooks.compilation.tap('ExposeWebpackRequirePlugin', (compilation) => {
+      compilation.mainTemplate.hooks.require.tap(
+        'ExposeWebpackRequirePlugin',
+        (source) => {
+          return `
+            window.__webpack_require__ = __webpack_require__;
+            ${source}
+          `;
+        },
+      );
+    });
+  }
+}
+
 export default defineConfig({
   publicPath: '/',
   routes: routes,
@@ -22,6 +39,9 @@ export default defineConfig({
     ['@babel/plugin-transform-class-properties', { loose: true }],
     'babel-plugin-parameter-decorator',
   ],
+  chainWebpack: (memo, args) => {
+    memo.plugin('expose-webpack-require').use(ExposeWebpackRequirePlugin);
+  },
   plugins: ['./dumi-plugin-nodenext', './umi-plugin-router', './umi-plugin-mana'],
   mfsu: false,
   jsMinifier: 'none',
