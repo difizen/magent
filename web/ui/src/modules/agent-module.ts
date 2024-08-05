ad4bb05eb7348611c242dee36e9fff93cac60adc
sunshinesmilelk <1176136681@qq.com>
2024-08-05 17:45:21 +0800

    refactor(ui): rename as tool

--- web/ui/src/modules/agent-module.ts
+++ web/ui/src/modules/agent-module.ts
@@ -3,11 +3,11 @@ import { ManaModule } from '@difizen/mana-app';
 import { AgentBotModule } from './agent/module.js';
 import { AxiosClientModule } from './axios-client/module.js';
 import { ModelModule } from './model/module.js';
-import { PluginModule } from './plugin/module.js';
+import { ToolModule } from './tool/module.js';
 
 export const AgentModule = ManaModule.create().dependOn(
   ModelModule,
   AgentBotModule,
   AxiosClientModule,
-  PluginModule,
+  ToolModule,
 );
---
+++ web/ui/src/modules/tool/index.ts
@@ -0,0 +1,6 @@
+export * from './protocol.js';
+export * from './tool.js';
+export * from './tool-manager.js';
+export * from './tool-config.js';
+export * from './tool-config-manager.js';
+export * from './module.js';
