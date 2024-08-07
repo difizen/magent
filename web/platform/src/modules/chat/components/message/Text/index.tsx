898a6510abf694758db443d5a0674299a56a3965
brokun <yukun.wyk@antgroup.com>
2024-08-07 11:03:15 +0800

    fix(ui): unexpected file name, fix step1

--- web/platform/src/modules/chat/components/message/Text/index.tsx
+++
@@ -1,9 +0,0 @@
-import './index.less';
-
-export const TextMessage = ({ content }: any) => {
-  return (
-    <div className={`text-message-text`}>
-      <span className={`text-message-textPop`}>{content}</span>
-    </div>
-  );
-};
