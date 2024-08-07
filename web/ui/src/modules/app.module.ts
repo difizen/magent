1b70a1f574d4d13dae041996e5beb257a8f05581
brokun <yukun.wyk@antgroup.com>
2024-08-08 02:28:17 +0800

    feat(ui): tools ui improvement

--- web/ui/src/modules/app.module.ts
+++ web/ui/src/modules/app.module.ts
@@ -8,14 +8,25 @@ import { PortalsModule } from '../views/protal-layout/index.js';
 import { SessionsViewModule } from '../views/sessions/module.js';
 import { ToolPageModule } from '../views/tool/module.js';
 
-import { AgentModule } from './agent-module.js';
+import { AgentBotModule } from './agent/module.js';
+import { AxiosClientModule } from './axios-client/module.js';
 import { BaseLayoutModule } from './base-layout/module.js';
 import { ChatMessageModule } from './chat-message/index.js';
+import { ModelModule } from './model/module.js';
 import { SessionModule } from './session/index.js';
+import { ToolModule } from './tool/module.js';
 
 export const AppBaseModule = new ManaModule()
   // 基础数据模块
-  .dependOn(BaseLayoutModule, AgentModule, SessionModule, ChatMessageModule)
+  .dependOn(
+    BaseLayoutModule,
+    SessionModule,
+    ChatMessageModule,
+    ModelModule,
+    AgentBotModule,
+    AxiosClientModule,
+    ToolModule,
+  )
   // 视图模块
   .dependOn(
     PortalsModule,
@@ -26,7 +37,6 @@ export const AppBaseModule = new ManaModule()
     ToolPageModule,
     AgentConfigViewModule,
     PortalsModule,
-    AgentsPageModule,
   );
 
 export default AppBaseModule;
