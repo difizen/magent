d7c0f32acff05803512f4ce912449b1c5fbcc95a
brokun <yukun.wyk@antgroup.com>
2024-08-26 16:33:48 +0800

    feat(ui): add resource upload api

--- packages/magent-ui/src/magent_ui/routers/main.py
+++ packages/magent-ui/src/magent_ui/routers/main.py
@@ -4,6 +4,7 @@ from magent_ui.routers.knowledge.router import knowledge_router
 from magent_ui.routers.llms.router import llms_router
 from magent_ui.routers.sessions.router import sessions_router
 from magent_ui.routers.tools.router import tools_router
+from magent_ui.routers.resource.router import resource_router
 
 api_router = APIRouter()
 
@@ -12,3 +13,4 @@ api_router.include_router(knowledge_router, prefix="/v1", tags=["knowledge"])
 api_router.include_router(llms_router, prefix="/v1", tags=["llm"])
 api_router.include_router(sessions_router, prefix="/v1", tags=["session"])
 api_router.include_router(tools_router, prefix="/v1", tags=["tool"])
+api_router.include_router(resource_router, prefix="/v1", tags=["resource"])
