c937c2127fcf58bafdc36bba780e860127129db7
brokun <yukun.wyk@antgroup.com>
2024-08-26 19:12:27 +0800

    chore(ui): hide agent creation entry

--- web/ui/src/views/agents/view.tsx
+++ web/ui/src/views/agents/view.tsx
@@ -16,6 +16,7 @@ import { history } from 'umi';
 import './index.less';
 import { AgentIcon } from '../../modules/agent/agent-icon.js';
 import { AgentMarket } from '../../modules/agent/agent-market.js';
+
 import { AgentCreateModal, agentCreateModalId } from './modal/create.js';
 
 const viewId = 'magent-agents';
@@ -64,7 +65,7 @@ const AgentsViewComponent = forwardRef<HTMLDivElement>(
           ))}
         </div>
 
-        <div className="magent-agents-creation">
+        {/* <div className="magent-agents-creation">
           <Button
             onClick={() => {
               modalService.openModal(AgentCreateModal);
@@ -73,7 +74,7 @@ const AgentsViewComponent = forwardRef<HTMLDivElement>(
           >
             创建智能体
           </Button>
-        </div>
+        </div> */}
       </div>
     );
   },
