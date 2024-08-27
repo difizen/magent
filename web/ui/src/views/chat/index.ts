70ab78009f6ec8a33b7221ccfe38a2fe6d8f075e
yukun.wyk <yukun.wyk@antgroup.com>
2024-08-28 01:54:38 +0800

    refactor: reorganize the project front-end directory

--- web/ui/src/views/chat/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './module.js';
-export * from './chat-manager.js';
-export * from './message-manager.js';
-export * from './view.js';
-export * from './chat-message.js';
-export * from './protocol.js';
