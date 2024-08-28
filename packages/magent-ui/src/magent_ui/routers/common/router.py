d1a3b7accb66548589ad7176ecc4229b332f0a57
yukun.wyk <yukun.wyk@antgroup.com>
2024-08-29 00:51:34 +0800

    fix(ui): migrate util api

--- packages/magent-ui/src/magent_ui/routers/common/router.py
+++ packages/magent-ui/src/magent_ui/routers/common/router.py
@@ -3,7 +3,7 @@ import enum
 import json
 from typing import AsyncIterable, List
 from fastapi import APIRouter
-import agentuniverse_product.service.util.product_util as util
+from agentuniverse_product.service.util.agent_util import is_component_id_unique
 
 router = APIRouter()
 common_router = router
@@ -11,6 +11,6 @@ common_router = router
 
 @router.get("/common/is_id_unique", response_model=bool)
 async def is_id_unique(id:str, type:str):
-    result = util.is_id_unique(id,type)
+    result = is_component_id_unique(id,type)
     return result
 
