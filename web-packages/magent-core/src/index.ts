984b4a02a000fde391b6cdd07bc0cb7b2eaf90f0
yukun.wyk <yukun.wyk@antgroup.com>
2024-09-21 22:15:48 +0800

    feat(core): prioritized contribution

--- web-packages/magent-core/src/index.ts
+++ web-packages/magent-core/src/index.ts
@@ -1,4 +1,5 @@
 export * from './common/async-model.js';
 export * from './common/utils.js';
+export * from './common/prioritized-contribution.js';
 export * from './fetcher/fetcher.js';
 export * from './fetcher/module.js';
