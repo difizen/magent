0c117008e40bc85590eb7f7dea8c304f2af8033f
sunshinesmilelk <1176136681@qq.com>
2024-10-28 14:45:17 +0800

    fix: export ImageModal

--- web-packages/magent-chat/src/chat-view/index.ts
+++ web-packages/magent-chat/src/chat-view/index.ts
@@ -2,12 +2,13 @@ export * from './module.js';
 export * from './view.js';
 
 import { Input } from './components/input/index.js';
-import { DefaultMarkdown } from './components/markdown/index.js';
+import { DefaultMarkdown, ImageModal } from './components/markdown/index.js';
 import { TextMessage } from './components/text/index.js';
 import { Typing } from './components/typing/index.js';
 
 export const ChatComponents = {
   Input,
+  ImageModal,
   Markdown: DefaultMarkdown,
   TextMessage,
   Typing,
