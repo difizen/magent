78e12e0857256a41331c4902e1a95b789425b721
xujingli.xjl <xujingli.xjl@antgroup.com>
2024-11-08 17:56:40 +0800

    fix: add qwen demo

--- packages/magent-ui-langchain/src/magent_ui_langchain/dev.py
+++ packages/magent-ui-langchain/src/magent_ui_langchain/dev.py
@@ -1,13 +1,17 @@
-from langchain_openai import ChatOpenAI
+# import sys
+# sys.path.append("/Users/johnny/Code/difizen/magent/packages/magent-ui-langchain/src")
+# from langchain_openai import ChatOpenAI
 from magent_ui_langchain import launch
 
-llm = ChatOpenAI(
-    model="gpt-4o",
+from langchain_community.chat_models import ChatTongyi
+
+tongyi_chat = ChatTongyi(
+    model="qwen-max",
     temperature=0,
     max_tokens=None,
     timeout=None,
     max_retries=2,
 )
 
+launch(tongyi_chat)
 
-launch(llm)
