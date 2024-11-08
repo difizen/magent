e8f6beb085391de151c4c8839474c6ce62f67072
yukun.wyk <yukun.wyk@antgroup.com>
2024-11-08 18:28:30 +0800

    fix(langchain): astream invoke

--- packages/magent-ui-langchain/src/magent_ui_langchain/routers/chat/router.py
+++ packages/magent-ui-langchain/src/magent_ui_langchain/routers/chat/router.py
@@ -44,9 +44,11 @@ class SSEType(enum.Enum):
     EOF = "EOF"
     RESULT = "result"
 
+
 async def async_generator_from_sync(gen):
     for item in gen:
         yield item
+
 
 async def send_message(model: MessageCreate, stream: bool) -> AsyncIterable[ServerSentEvent]:
     current = get_current_executor()
@@ -54,24 +56,28 @@ async def send_message(model: MessageCreate, stream: bool) -> AsyncIterable[Serv
         yield ServerSentEvent(event=SSEType.ERROR.value, id=model.conversation_id, data=json.dumps({"error_message": "error executor"}, ensure_ascii=False))
         return
     if isinstance(current, StreamExecutor) and stream:
-        msg_iterator = async_generator_from_sync(current.invoke_stream(model.input))
+        msg_iterator = current.invoke_stream(model.input)
         if msg_iterator is None:
             yield ServerSentEvent(event=SSEType.ERROR.value, id=model.conversation_id, data=json.dumps({"error_message": "error stream invoke_stream"}, ensure_ascii=False))
             return
         try:
-          async for msg_chunk in msg_iterator:
-              if isinstance(msg_chunk, AIMessageChunk):
-                if hasattr(msg_chunk, 'response_metadata') and len(msg_chunk.response_metadata) > 0: # 最后一次返回
-                  yield ServerSentEvent(event=SSEType.RESULT.value, id=model.conversation_id, data=json.dumps({"output": msg_chunk.content, "id": msg_chunk.id, "response_metadata": msg_chunk.response_metadata}, ensure_ascii=False))
-                else:
-                  yield ServerSentEvent(event=SSEType.CHUNK.value, id=model.conversation_id, data=json.dumps({"output": msg_chunk.content, "id": msg_chunk.id}, ensure_ascii=False))
+            async for msg_chunk in msg_iterator:
+                print(msg_chunk)
+                if isinstance(msg_chunk, AIMessageChunk):
+                    # 最后一次返回
+                    if msg_chunk.response_metadata is not None and len(msg_chunk.response_metadata) > 0:
+                        yield ServerSentEvent(event=SSEType.RESULT.value, id=model.conversation_id, data=json.dumps({"output": msg_chunk.content, "id": msg_chunk.id, "response_metadata": msg_chunk.response_metadata}, ensure_ascii=False))
+                    else:
+                        yield ServerSentEvent(event=SSEType.CHUNK.value, id=model.conversation_id, data=json.dumps({"output": msg_chunk.content, "id": msg_chunk.id}, ensure_ascii=False))
         except Exception as e:
-          yield ServerSentEvent(event=SSEType.ERROR.value, id=model.conversation_id, data=json.dumps({"error_message": "error in stream execute"}, ensure_ascii=False))
-          return
+            yield ServerSentEvent(event=SSEType.ERROR.value, id=model.conversation_id, data=json.dumps({"error_message": "error in stream execute"}, ensure_ascii=False))
+            return
+
 
 @router.post("/chat-stream")
 async def stream_chat(model: MessageCreate):
     return EventSourceResponse(send_message(model, stream=True), media_type="text/event-stream")
+
 
 @router.post("/chat")
 async def chat(model: MessageCreate):
@@ -80,10 +86,9 @@ async def chat(model: MessageCreate):
         return {"error_message": "error executor"}
 
     try:
-      result = current.invoke(model.input)
-      output_dict = { 'id': result.id, 'output': result.content, 'response_metadata': result.response_metadata}
-      return output_dict
+        result = current.invoke(model.input)
+        output_dict = {'id': result.id, 'output': result.content,
+                       'response_metadata': result.response_metadata}
+        return output_dict
     except Exception as e:
-      return {"error_message": "chat execute error"}
-
-
+        return {"error_message": "chat execute error"}
